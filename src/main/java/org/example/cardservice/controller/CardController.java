a8dc614b99d4484f910faf7e9e6a78b1505fe9d0
alyvnihad <aliyevnihad011@gmail.com>
2025-09-30 18:19:04 +0400

    Card Controller register update.v1 commit - Card Service

--- src/main/java/org/example/cardservice/controller/CardController.java
+++ src/main/java/org/example/cardservice/controller/CardController.java
@@ -3,6 +3,7 @@ package org.example.cardservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.cardservice.dto.AccountRequest;
 import org.example.cardservice.service.CardService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CardController {
     private final CardService cardService;
 
+    @PostMapping("register")
     public void register(@RequestBody AccountRequest request){
         cardService.register(request.getPinHash(), request.getCurrency());
     }

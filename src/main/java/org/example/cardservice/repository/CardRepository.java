bf5275560914d2a4effdf3ec8f84f998045c76ac
alyvnihad <aliyevnihad011@gmail.com>
2025-09-30 18:43:24 +0400

    Card Repository update.v1 commit - Card Service

--- src/main/java/org/example/cardservice/repository/CardRepository.java
+++ src/main/java/org/example/cardservice/repository/CardRepository.java
@@ -3,6 +3,10 @@ package org.example.cardservice.repository;
 import org.example.cardservice.model.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface CardRepository extends JpaRepository<Card,Long> {
+    Optional<Card> findByIdAndCardNumber(Long id, Long cardNumber);
+    Card findByCardNumber(Long cardNumber);
 }

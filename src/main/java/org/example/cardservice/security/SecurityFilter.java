b3707c299f4ad2f85ff8218b0ede3628a93dfbb9
alyvnihad <aliyevnihad011@gmail.com>
2025-10-06 19:29:39 +0400

    SecurityFilter update.v2 commit - Card Service

--- src/main/java/org/example/cardservice/security/SecurityFilter.java
+++ src/main/java/org/example/cardservice/security/SecurityFilter.java
@@ -7,32 +7,34 @@ import org.example.cardservice.model.PaymentNetwork;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Component
 @RequiredArgsConstructor
 public class SecurityFilter {
 
+    private static final AtomicLong counter = new AtomicLong(0);
     private final Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
 
     public String passwordEncoder(String pin) {
-       return argon2.hash(4, 65536, 3, pin);
+        return argon2.hash(4, 65536, 3, pin);
     }
 
-    public boolean passwordDecoder(String dbPinHashed,String enteredPin){
-        return argon2.verify(dbPinHashed,enteredPin);
+    public boolean passwordDecoder(String dbPinHashed, String enteredPin) {
+        return argon2.verify(dbPinHashed, enteredPin);
     }
 
-    public Long cardNumber(){
-        Long value = generatedValue();
-        return 12341234 + value;
+    public Long cardNumber() {
+        long base = 12341234L * 10000L * 10000L;
+        return  base + generatedValue();
     }
 
-    protected Long generatedValue(){
-        return System.currentTimeMillis()/100000;
+    protected Long generatedValue() {
+        return System.currentTimeMillis() / 100000 + counter.getAndIncrement();
     }
 
-    public PaymentNetwork generatedPaymentNetwork(){
+    public PaymentNetwork generatedPaymentNetwork() {
         PaymentNetwork[] values = PaymentNetwork.values();
         Random random = new Random();
         int index = random.nextInt(values.length);

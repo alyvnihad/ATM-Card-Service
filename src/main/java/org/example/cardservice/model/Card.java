85c474c88d2f9d5cb3858dfc16af18d49605fbac
alyvnihad <aliyevnihad011@gmail.com>
2025-10-23 11:57:25 +0400

    Model class update.v3 commit - Card Service

--- src/main/java/org/example/cardservice/model/Card.java
+++ src/main/java/org/example/cardservice/model/Card.java
@@ -13,7 +13,6 @@ public class Card {
     private Long id;
     private Long cardNumber;
     private LocalDate expiryDate;
-    //cvv kodda lazimdi
     private String pinHash;
     private int failedPin;
     @Enumerated(value = EnumType.STRING)
